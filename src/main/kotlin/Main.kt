4ecef0dc7d4505462040e1564f6c875cff03f946
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-03-08 16:30:27 +0300

    inheritance

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main(){
+ var car = Car("volkswagen","sedan","grey",6)
+   car.carry(6)
+   car.carry(10)
+    car.identity()
+    println(car.calculateParkingfees(3))
+
+    var bus = Bus("scania","toyota","white",2)
+   println(bus.maxTripFare(30.0))
+
+}
+open class Car( var make:String,var model:String, var color:String, var capacity: Int){
+
+    fun carry(people: Int){
+
+      var x = people-capacity
+        if(capacity>=people){
+          println("carrying $people passengers")}
+        else{
+            println("over capacity by $x people")
+        }
+            }
+    fun identity(){
+        println("i am a $color $make $model ")
+    }
+    open fun calculateParkingfees(hours: Int): Int {
+       var parkingfees= hours*20
+        return parkingfees
+    }
+        }
+ class Bus( make: String,model: String,color: String, capacity: Int) :Car(make,model,color,capacity){
+
+     fun maxTripFare(fare: Double): Double{
+         return fare * capacity
+     }
+
+     override fun calculateParkingfees(hours: Int): Int {
+         return super.calculateParkingfees(hours)
+     }
+
+ }
+
